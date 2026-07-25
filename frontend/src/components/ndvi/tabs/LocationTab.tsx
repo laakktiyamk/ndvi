5d4794b12c867eeaa9dd7e1a54adf72f4473022b
laakktiyamk <laakkti.yamk@gmail.com>
2026-07-25 21:46:46 +0300

    added leaflet icon

--- frontend/src/components/ndvi/tabs/LocationTab.tsx
+++ frontend/src/components/ndvi/tabs/LocationTab.tsx
@@ -4,6 +4,19 @@ import { Box, Typography, Alert } from '@mui/material';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
+
+
 interface Props {
   geometry: { type: string; coordinates: unknown[] };
   fieldName?: string;
